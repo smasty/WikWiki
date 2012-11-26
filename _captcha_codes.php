6c8ed61ee585c104aa71225934d050b1dc568de5
Karel Bílek <kb@karelbilek.com>
2012-11-26 20:32:21 +0100

    Adding reCaptcha

---
+++ _captcha_codes.php
@@ -0,0 +1,14 @@
+<?php
+
+#Write your captcha keys here
+#and move it to captcha_codes.php
+#(remove the first underscore
+
+function get_public_recaptcha_key() {
+    return "publicRecaptchaKey123";
+}
+function get_private_recaptcha_key() {
+    return "privateRecaptchaKey123";
+}
+
+?>
